cc75b4ddc00a29ba1546e01de9564bcd91e42fa6
Charly Mannion <charlottemannion100@gmail.com>
2018-06-26 09:46:03 +0100

    Initial Commit

---
+++ Swift-TwilioTests/Swift_TwilioTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_TwilioTests.swift
+//  Swift-TwilioTests
+//
+//  Created by Charly Mannion on 26/06/2018.
+//  Copyright © 2018 Charly Mannion. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Twilio
+
+class Swift_TwilioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
