554ba67bb05021de6120f63ebfe2b7ff3eb50099
Muzzi Aldean <amiraldean@gmail.com>
2018-06-26 11:08:16 +0100

    Test SMS sending

--- Sources/Swift-Twilio/main.swift
+++ Sources/Swift-Twilio/main.swift
@@ -5,11 +5,11 @@ if let accountSID = ProcessInfo.processInfo.environment["TWILIO_ACCOUNT_SID"],
     let authToken = ProcessInfo.processInfo.environment["TWILIO_AUTH_TOKEN"] {
     
     let url = "https://api.twilio.com/2010-04-01/Accounts/\(accountSID)/Messages"
-    let parameters = ["From": "+", "To": "+", "Body": "Send noodz"]
+    let parameters = ["From": "+441423740326", "To": "+447891721675", "Body": "Send noodz"]
     
     Alamofire.request(url, method: .post, parameters: parameters)
         .authenticate(user: accountSID, password: authToken)
-        .responseJSON { response in
+        .responseString { response in
             debugPrint(response)
     }
     
